2ac162de1169b46303a4dcebe1e12422028e8e1b
陳建佑 <jianyou224@gmail.com>
2022-05-18 23:47:42 +0800

    hover transform

--- js/all.js
+++ js/all.js
@@ -1,17 +1,16 @@
+//滾動到指定位置顯示元素------------------------------
 window.addEventListener('scroll',reveal);
 function reveal(){
   const reveals = document.querySelectorAll('.reveal');
-  reveals.forEach((item,i)=>{
-    let windowHeight = window.innerHeight;
-    let revealTop = reveals[i].getBoundingClientRect().top;
-    let revealPoint = 150;
+  reveals.forEach((item,index)=>{
+    let windowHeight = window.innerHeight;//視窗高度
+    let itemHeight = reveals[index].getBoundingClientRect().top;//item到頂部的距離
     
-    if(revealTop < windowHeight - revealPoint){
-      reveals[i].classList.add('show-section');
+    if(itemHeight < windowHeight/1.2){
+      reveals[index].classList.add('show-section');
     }else{
-      reveals[i].classList.remove('show-section');
-      // return
+      return
     }
-    // console.log(i)
+    // console.log(windowHeight,`/ 距離頂部:${itemHeight}`)
   })
 }
